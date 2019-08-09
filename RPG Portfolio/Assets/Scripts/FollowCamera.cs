8ba58f55fceea6faba4b36b6da60721824495d97
Mr.StevenKim <dev@donga.ac.kr>
2019-08-09 20:36:57 +0900

    19.08.09
    전에 만들던게 GetKey가 안먹히는 문제가 생겨 게임의 틀은 그대로 둔대로 약간의 컨셉 변경 및 게임 그래픽 변경...

---
+++ RPG Portfolio/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+
+    Transform target;
+    public float Dist;
+    public float Height;
+    public float SmoothRotate;
+
+    Transform camera;
+
+    public void Set_Target(GameObject _input)
+    {
+        target = _input.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Player.transform;
+        camera = GetComponent<Transform>();
+    }
+
+    private void LateUpdate()
+    {
+       
+        if(target != null)
+        {
+            float curraYAngle = Mathf.LerpAngle(camera.eulerAngles.y, camera.eulerAngles.y, SmoothRotate * Time.deltaTime);
+
+            Quaternion rot = Quaternion.Euler(0, curraYAngle, 0);
+
+            camera.transform.position = target.position - (rot * Vector3.forward * Dist) + (Vector3.up * (Height));
+
+            camera.transform.LookAt(target);
+        }
+    }
+}
---
+++ RPG Portfolio/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator Anim;
+
+    public float MoveSpeed;
+    public float RotSpeed;
+    bool MoveFlag = false;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCtrl();
+        RotCtrl();
+    }
+    void MoveCtrl()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Input W");
+            MoveFlag = true;
+            this.transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            MoveFlag = false;
+        }
+    }
+
+    void RotCtrl()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            float RotX = Input.GetAxis("Mouse Y") * RotSpeed;
+            float RotY = Input.GetAxis("Mouse X") * RotSpeed;
+
+            this.transform.localRotation *= Quaternion.Euler(0, RotY, 0);
+        }
+    }
+}
