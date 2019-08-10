1d248099824ea738ead3c01b70b225dc7cae6101
Mr.StevenKim <dev@donga.ac.kr>
2019-08-11 01:30:56 +0900

    19.08.11

---
+++ RPG Portfolio/Assets/Scripts/Network/PacketQueue.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+
+
+public class PacketQueue 
+{
+    struct PacketInfo // 패킷 저장 정보
+    {
+        public int offset;
+        public int size;
+
+    };
+    
+
+    private MemoryStream m_streamBuffer; // 데이터를 보존할 버퍼
+
+    private List<PacketInfo> m_offsetList; // 패킷 정보 관리 리스트
+
+    private int m_offset = 0;// 메모리 배치 오프셋
+
+    private Object lockObj = new Object(); // 락 오브젝트
+
+    public PacketQueue() // 생성자 (여기서 초기화시킴)
+    {
+        m_streamBuffer = new MemoryStream();
+        m_offsetList = new List<PacketInfo>();
+    }
+    
+    public int Enqueue(byte[] data, int size) // 큐에 데이터 추가
+    {
+        PacketInfo info = new PacketInfo();
+
+        // 패킷 정보 작성
+        info.offset = m_offset;
+        info.size = size;
+
+        lock(lockObj)
+        {
+            m_offsetList.Add(info); // 패킷 저장 정보 보존
+
+            m_streamBuffer.Position = m_offset; // 패킷 데이터 보존
+            m_streamBuffer.Write(data, 0, size);
+            m_streamBuffer.Flush();
+            m_offset += size;
+        }
+
+        return size;
+    }
+
+    public int Dequeue(ref byte[] buffer, int size) // 큐에서 데이터 리턴
+    {
+        if(m_offsetList.Count <= 0)
+        {
+            return -1;
+
+        }
+
+        int recvSize = 0;
+
+        lock(lockObj)
+        {
+            PacketInfo info = m_offsetList[0]; // 버퍼에서 해당하는 패킷 데이터를 가져 온다.
+            int dataSize = Math.Min(size, info.size);
+            m_streamBuffer.Position = info.offset;
+            recvSize = m_streamBuffer.Read(buffer, 0, dataSize);
+
+            if(recvSize > 0)// 큐 데이터를 앞으로 꺼냈으므로 맨 앞 데이터는 삭제
+            {
+                m_offsetList.RemoveAt(0);
+
+            }
+
+            //모든 큐 데이터를 꺼냈을 때는 스트림을 정리해서 메모리를 절약한다.
+            if(m_offsetList.Count ==0)
+            {
+                Clear();
+                m_offset = 0;
+            }
+
+        }
+        return recvSize;
+    }
+
+    public void Clear()
+    {
+        byte[] buffer = m_streamBuffer.GetBuffer();
+        Array.Clear(buffer, 0, buffer.Length);
+
+        m_streamBuffer.Position = 0;
+        m_streamBuffer.SetLength(0);
+    }
+}
---
+++ RPG Portfolio/Assets/Scripts/Network/TransportTCP.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransportTCP : MonoBehaviour
+{
+    Socket m_listener;
+    bool m_isServer;
+    Socket m_socket;
+    bool m_isConnected;
+   public bool StartServer(int port, int connectionNum)
+    {
+        // 리스닝 소켓 생성
+        m_listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        m_listener.Bind(new IPEndPoint(IPAddress.Any, port));
+        m_listener.Listen(connectionNum);
+        m_isServer = true;
+
+        return true;
+    }
+
+    public void StopServer()
+    {
+        // 리스닝 소켓 닫기
+        m_listener.Close();
+        m_listener = null;
+        m_isServer = false;
+    }
+
+    public bool Connect(string address, int port)
+    {
+       
+        // 소켓 연결
+
+        m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        m_socket.NoDelay = true;
+        m_socket.Connect(address, port);
+        m_socket.SendBufferSize = 0;
+        m_isConnected = true;
+
+        return true;
+    }
+
+    public bool Disconnect()
+    {
+        // 소켓을 닫는다.
+        m_socket.Shutdown(SocketShutdown.Both);
+        m_socket.Close();
+        m_socket = null;
+
+        m_isConnected = false;
+
+
+        return true;
+    }
+
+    private PacketQueue m_sendQueue = new PacketQueue(); // 송신 버퍼
+    private PacketQueue m_recvQueue = new PacketQueue(); // 수신 버퍼
+
+    public int Send(byte[] data, int size)
+    {
+        return m_sendQueue.Enqueue(data, size);
+    }
+
+    public int Receive(ref byte[] buffer, int size)
+    {
+        return m_recvQueue.Dequeue(ref buffer, size);
+    }
+
+    
+    
+}
